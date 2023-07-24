7b67aebce9d7ffc741c97cde7eb3384e31baced5
Alex Knight <alex@alexknight.co.uk>
2023-07-24 20:20:41 +0100

    Add .gitignore & removed unneeded packages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,7 @@ const config: GatsbyConfig = {
     // If you use VSCode you can also use the GraphQL plugin
     // Learn more at: https://gatsby.dev/graphql-typegen
     graphqlTypegen: true,
-    plugins: ["gatsby-plugin-postcss", "gatsby-plugin-styled-components",
+    plugins: ["gatsby-plugin-styled-components",
         `gatsby-plugin-image`,
         `gatsby-plugin-sharp`,
         `gatsby-transformer-sharp`, "gatsby-plugin-sitemap", "gatsby-transformer-yaml", {
